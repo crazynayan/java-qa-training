f47800b8a58b3678a0862116ac5f149511f91ea3
crazynayan <nayan@crazyideas.co.in>
2019-08-12 19:24:57 +0530

    CalculatorTest

---
+++ src/com/company/CalculatorTest.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    private Calculator calc;
+
+    @BeforeEach
+    void setUp() {
+        calc = new Calculator();
+    }
+
+    @Test
+    void getNumber() {
+        assertEquals(0, calc.getNumber());
+    }
+}
