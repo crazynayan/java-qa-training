927c5a7c2f2b381f62b56ec777c78ede3e7279dd
crazynayan <nayan@crazyideas.co.in>
2019-08-24 16:40:44 +0530

    Ready to accept state and executable exceptions

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -2,16 +2,27 @@ package com.company;
 
 class Calculator {
     private int number;
+    private boolean readyToAccept = true;
 
     int getNumber() {
         return number;
     }
 
+    boolean isReadyToAccept() {
+        return readyToAccept;
+    }
+
     void setNumber(int number) {
-        this.number = number;
+        if (readyToAccept) {
+            this.number = number;
+            readyToAccept = false;
+        } else {
+            throw new ArithmeticException();
+        }
     }
 
     void clear() {
         number = 0;
+        readyToAccept = true;
     }
 }
--- src/com/company/CalculatorTest.java
+++ src/com/company/CalculatorTest.java
@@ -2,6 +2,7 @@ package com.company;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -53,6 +54,26 @@ class CalculatorTest {
     void testSmallestNumber() {
         calc.setNumber(-2147483648);
         assertEquals(-2147483648, calc.getNumber());
+    }
+
+    @Test
+    void testMultipleNumberInputWithClear() {
+        calc.setNumber(25);
+        calc.clear();
+        calc.setNumber(56);
+        assertEquals(56, calc.getNumber());
+    }
+
+    @Test
+    void testMultipleNumberWithoutClear() {
+        calc.setNumber(25);
+        Executable setNumberWith56 = () -> calc.setNumber(56);
+        assertThrows(ArithmeticException.class, setNumberWith56);
+    }
+
+    @Test
+    void testInitialReadyToAcceptIsTrue() {
+        assertTrue(calc.isReadyToAccept());
     }
 
 }
